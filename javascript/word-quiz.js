d334b85ce738383bfa65f9c861ed6da9b499a763
bichu136 <bichu136>
2021-12-03 19:21:08 +0700

    change assets folder, done result screenfor word quiz

--- javascript/word-quiz.js
+++ javascript/word-quiz.js
@@ -3,19 +3,56 @@
 //spell
 //_type
 //kanji
-
+//correct_image
+//failed_image
 let choices = document.getElementsByClassName("choice");
 let correct_screen = document.getElementById("finished");
 let answers_div = document.getElementById("answers");
 let result_word = document.getElementById("result-word");
+let correct_answers = document.getElementById("correct_answers");
+console.log('checking_something');
+for(var i=0;i<correct_image.length;i+=1){
+    console.log(correct_image[i]);
+}
+function get_random_number(max){
+     return Math.floor(Math.random()*max);
+}
 function view_correct_screen(answers){
-    word_div.style.filter = "blur(3px)";
-    answers_div.style.filter = "blur(3px)";
+
+    word_div.style.filter = "blur(5px)";
+    answers_div.style.filter = "blur(5px)";
+    get_surrounding_image(answers);
     correct_screen.style.visibility=null;
-    result_word.textContent =answers;
+    let return_string = answers? "correct": "failed";
+    let correct_string = correct_kanji+"|"+correct_spell+"|"+correct_meaning+"|"+correct_type ;
+    result_word.textContent =return_string;
+    correct_answers.textContent =correct_string;
+}
+function get_surrounding_image(answers){
+    var top_left_div     = document.getElementById("image-top-left");
+    var top_right_div    = document.getElementById("image-top-right");
+    var top_div          = document.getElementById("image-top");
+    var left_div         = document.getElementById("image-left");
+    var right_div        = document.getElementById("image-right");
+    var bottom_left_div  = document.getElementById("image-bottom-left");
+    var bottom_right_div = document.getElementById("image-bottom-right");
+    var bottom_div       = document.getElementById("image-bottom");
+    var arr = [top_left_div,top_div,top_right_div,left_div,right_div,bottom_left_div,bottom_div,bottom_right_div];
+    var i=0;
+
+    if (answers){
+        var list_image = correct_image;
+    }
+    else{
+        var list_image = failed_image;
+    }
+    while (i<arr.length){
+        arr[i].children[0].src = list_image[get_random_number(list_image.length)];
+        i+=1;
+    }
 }
 for(var i= 0; i <choices.length;i++){
-choices[i].onclick = function(event){event.path[0].textContent== result ? view_correct_screen("correct"):view_correct_screen("failed");};
+choices[i].onclick = function(event){event.target.textContent== result ? view_correct_screen(true):view_correct_screen(false);};
 }
 let passing_words = []
 //  random word index
